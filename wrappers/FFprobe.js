e6701d900854b4b7e45767f22173ee79903a736d
Josh Curtiss <git@curtiss.me>
2017-03-10 23:46:45 -0600

    Added sync versions of methods.
    Reorganized to efficiently have async and sync versions of createWebVTT() and getInfo() methods.

--- wrappers/FFprobe.js
+++ wrappers/FFprobe.js
@@ -12,41 +12,45 @@ class FFprobe {
 
     createWebVTT(videoPath,cb) {
         exec(`${this.executablePath} -show_chapters -of json ${videoPath}`,(err,stdout,stderr)=>{
-            if( err ) {
-                cb(err,null);
-            } else {
-                var data=JSON.parse(stdout);
-                var chapters=data.chapters;
-                // Loop thru chapters to adjust data
-                for( var chapter of chapters ) {
-                    // Parse the start/end values
-                    chapter.start=parseFloat(chapter.start_time);
-                    chapter.end=parseFloat(chapter.end_time);
-                    // Pull out the tags
-                    for( var tag in chapter.tags )
-                        chapter[tag]=chapter.tags[tag];
-                }
-                // Put the data into WebVTT object
-                var webvtt=new WebVTT(chapters);
-                cb(null,webvtt);
-            }
+            if( err ) cb(err,null);
+            else cb(null,this._createWebVTT(stdout));
         });
+    }
+    createWebVTTSync(videoPath) {
+        return this._createWebVTT(execSync(`${this.executablePath} -show_chapters -of json ${videoPath}`));
+    }
+    _createWebVTT(json) {
+        var data=JSON.parse(json);
+        var chapters=data.chapters;
+        // Loop thru chapters to adjust data
+        for( var chapter of chapters ) {
+            // Parse the start/end values
+            chapter.start=parseFloat(chapter.start_time);
+            chapter.end=parseFloat(chapter.end_time);
+            // Pull out the tags
+            for( var tag in chapter.tags )
+                chapter[tag]=chapter.tags[tag];
+        }
+        // Return a WebVTT object
+        return new WebVTT(chapters);
     }
 
     getInfo(videoPath,cb) {
         exec(`${this.executablePath} -show_format -of json ${videoPath}`,(err,stdout,stderr)=>{
-            if( err ) {
-                cb(err,null);
-            } else {
-                var data=JSON.parse(stdout);
-                var info={
-                    "start": parseFloat(data.format["start_time"]),
-                    "duration": parseFloat(data.format["duration"]),
-                    "bitrate": parseFloat(data.format["bit_rate"])
-                };
-                cb(null,info);
-            }
+            if( err ) cb(err,null);
+            else cb(null,this._getInfo(stdout));
         });
+    }
+    getInfoSync(videoPath) {
+        return this._getInfo(execSync(`${this.executablePath} -show_format -of json ${videoPath}`));
+    }
+    _getInfo(json) {
+        var data=JSON.parse(json);
+        return {
+            "start": parseFloat(data.format["start_time"]),
+            "duration": parseFloat(data.format["duration"]),
+            "bitrate": parseFloat(data.format["bit_rate"])
+        };
     }
 
     executableExists() {
--- wrappers/MPlayer.js
+++ wrappers/MPlayer.js
@@ -11,46 +11,51 @@ class MPlayer {
     }
 
     createWebVTT(videoPath,cb) {
-        var match, data={};
         exec(`${this.executablePath} -vo null -ao null -identify -frames 0 ${videoPath}`,(err,stdout,stderr)=>{
-            if( err ) {
-                cb(err,null);
-            } else {
-                // Build a map by finding all matches in mplayer's info output
-                while( match=MPlayer.CHAPTER_REGEX.exec(stdout) ) {
-                    var id=match[1],
-                        key=match[2].toLowerCase(),
-                        value=match[3];
-                    if( !data[id] ) data[id]={id:id};
-                    if( key=="start" || key=="end" ) value=parseFloat(value)/1000;
-                    data[id][key]=value;
-                }
-                // Convert the map to a list
-                var arr=[];
-                for( var objkey in data ) {arr.push(data[objkey])}
-                // Put the data into WebVTT object
-                var webvtt=new WebVTT(arr);
-                cb(null,webvtt);
-            }
+            if( err ) cb(err,null);
+            else cb(null,this._createWebVTT(stdout));
         });
+    }
+    createWebVTTSync(videoPath) {
+        return this._createWebVTT(execSync(`${this.executablePath} -vo null -ao null -identify -frames 0 ${videoPath}`));
+    }
+    _createWebVTT(txt) {
+        var match, data={};
+        // Build a map by finding all matches in mplayer's info output
+        while( match=MPlayer.CHAPTER_REGEX.exec(txt) ) {
+            var id=match[1],
+                key=match[2].toLowerCase(),
+                value=match[3];
+            if( !data[id] ) data[id]={id:id};
+            if( key=="start" || key=="end" ) value=parseFloat(value)/1000;
+            data[id][key]=value;
+        }
+        // Convert the map to a list
+        var arr=[];
+        for( var objkey in data ) {arr.push(data[objkey])}
+        // Return a WebVTT object
+        return new WebVTT(arr);
     }
 
     getInfo(videoPath,cb) {
         exec(`${this.executablePath} -vo null -ao null -identify -frames 0 ${videoPath}`,(err,stdout,stderr)=>{
-            if( err ) {
-                cb(err,null);
-            } else {
-                var findStart=stdout.match(/^id_start_time=([\d.]+)$/im);
-                var findDuration=stdout.match(/^id_length=([\d.]+)$/im);
-                var findBitrate=stdout.match(/^id_video_bitrate=([\d.]+)$/im);
-                var info={
-                    "start": findStart.length>1?parseFloat(findStart[1]):null,
-                    "duration": findDuration.length>1?parseFloat(findDuration[1]):null,
-                    "bitrate": findBitrate.length>1?parseFloat(findBitrate[1]):null
-                };
-                cb(null,info);
-            }
+            if( err ) cb(err,null);
+            else cb(null,this._getInfo(stdout));
         });
+    }
+    getInfoSync(videoPath) {
+        return this._getInfo(execSync(`${this.executablePath} -vo null -ao null -identify -frames 0 ${videoPath}`));
+    }
+    _getInfo(txt) {
+        var string=new String(txt);
+        var findStart=string.match(/^id_start_time=([\d.]+)$/im);
+        var findDuration=string.match(/^id_length=([\d.]+)$/im);
+        var findBitrate=string.match(/^id_video_bitrate=([\d.]+)$/im);
+        return {
+            "start": findStart.length>1?parseFloat(findStart[1]):null,
+            "duration": findDuration.length>1?parseFloat(findDuration[1]):null,
+            "bitrate": findBitrate.length>1?parseFloat(findBitrate[1]):null
+        };
     }
 
     executableExists() {
